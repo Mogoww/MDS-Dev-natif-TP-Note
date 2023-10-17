251f91be7e650806ab023b054277e7c3e8ce5be0
Gauthier <gauthier.mauche@gmail.com>
2023-10-17 10:33:56 +0200

    ✨ add aleatoire note

--- app/src/main/java/com/example/noteproject/MainActivity.kt
+++ app/src/main/java/com/example/noteproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.noteproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.SearchView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
@@ -12,14 +13,13 @@ import com.example.noteproject.adapter.NoteAdapter
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        // set "csrjfrjldf" in textView id
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // set liste example Notes
-        val notes = listOf(
+        var notes = listOf(
             Note("title1", "description1"),
             Note("title2", "description2"),
             Note("title3", "description3")
@@ -28,27 +28,34 @@ class MainActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         val adapter = NoteAdapter(notes)
 
-        // Utilisation de GridLayoutManager pour afficher les notes en 2 colonnes (vous pouvez ajuster le nombre)
-        val layoutManager = GridLayoutManager(this, 2)
-        recyclerView.layoutManager = layoutManager
+        //val layoutManager = GridLayoutManager(this, 2)
+        //recyclerView.layoutManager = layoutManager
 
         recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         val searchView: SearchView = findViewById(R.id.search_view)
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                // Gérer la soumission de la recherche ici
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                // Gérer les modifications de texte de recherche ici
-                // Vous pouvez filtrer les éléments de la liste en fonction de newText
                 adapter.filter(newText)
                 return true
             }
         })
+
+
+        val addNoteButton: Button = findViewById(R.id.add_note_button)
+        addNoteButton.setOnClickListener {
+           notes = notes + Note(
+               "title ${notes.size + 1}",
+               "description ${notes.size + 1}"
+           )
+                adapter.updateNotes(notes)
+        }
 
     }
 }
--- app/src/main/java/com/example/noteproject/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/noteproject/adapter/NoteAdapter.kt
@@ -11,32 +11,42 @@ import com.example.noteproject.R
 class NoteAdapter(private var notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
     private var originalNotes: List<Note> = notes // Stockez la liste originale
 
+    // Créez une classe interne pour contenir les éléments de la vue
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.note_title)
         val contentTextView: TextView = itemView.findViewById(R.id.note_content)
     }
 
+    // Créez une vue pour chaque élément de la liste
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
         return NoteViewHolder(itemView)
     }
 
+    // Remplissez les données de la vue
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val currentNote = notes[position]
         holder.titleTextView.text = currentNote.title
         holder.contentTextView.text = currentNote.description
     }
 
+    // Retourne le nombre d'éléments dans la liste
     override fun getItemCount(): Int {
         return notes.size
     }
 
+    // Mettez à jour la liste de notes et la liste originale
+    fun updateNotes(newNotes: List<Note>) {
+        notes = newNotes
+        originalNotes = newNotes
+        notifyDataSetChanged()
+    }
+
+    // Filtrer les notes en fonction du texte de recherche
     fun filter(query: String?) {
         if (query.isNullOrBlank()) {
-            // Si le texte de recherche est vide, affichez toutes les notes
             notes = originalNotes
         } else {
-            // Sinon, filtrez les notes en fonction du texte de recherche
             notes = originalNotes.filter {
                 it.title.contains(query, ignoreCase = true) || it.description.contains(query, ignoreCase = true)
             }
